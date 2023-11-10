fd4d6044adbcc4374fdbe9f70e191a73411ebc47
sydneyjohann2 <sydneyjohann.laylo@gmail.com>
2023-11-10 12:50:24 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -28,5 +28,13 @@ CREATE TABLE animals (
     FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
 
+CREATE TABLE doctors(
+	doctorid INT PRIMARY KEY,
+	dfirstname VARCHAR(50) NOT NULL,
+	dlastname VARCHAR(50) NOT NULL,
+	specialty VARCHAR(100) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
+	email VARCHAR(100) NOT NULL
+);
 
 
